fe9e9ab3c3574b5dcff00b9f3b86a34dfe4a5c02
ameerthehacker <ameerjhanprof@gmail.com>
2020-03-27 17:05:02 +0530

    fix: breaking tests

--- src/parser/parser.spec.ts
+++ src/parser/parser.spec.ts
@@ -1,7 +1,9 @@
 import { interpolate } from './parser';
 
 describe('interpolate()', () => {
-  expect(
-    interpolate('%password%-%password%', { password: 'secret-password' })
-  ).toBe('secret-password-secret-password');
+  it('should interpolate variables', () => {
+    expect(
+      interpolate('%password%-%password%', { password: 'secret-password' })
+    ).toBe('secret-password-secret-password');
+  });
 });
