fa5037619980fc36580f550498b1ab59a24781bb
ameerthehacker <ameerjhanprof@gmail.com>
2020-03-27 19:29:44 +0530

    feat: replace carbon design by chakra ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './screens/app/app';
-import './index.scss';
+import { ThemeProvider, ColorModeProvider, CSSReset } from '@chakra-ui/core';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider>
+    <ColorModeProvider>
+      <CSSReset />
+      <App />
+    </ColorModeProvider>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
--- src/screens/app/app.tsx
+++ src/screens/app/app.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Button } from 'carbon-components-react';
+import { Button } from '@chakra-ui/core';
 
 export default function App() {
   return (
-    <Button>Hello</Button>
+    <Button variantColor="teal">Hello</Button>
   );
 }
