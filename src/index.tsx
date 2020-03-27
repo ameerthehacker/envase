c7afbf30b1c3f939e74683f44455847d3d8ff641
ameerthehacker <ameerjhanprof@gmail.com>
2020-03-27 17:00:14 +0530

    chore: add carbon design system

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './screens/app/app';
+import './index.scss';
 
-ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/screens/app/app.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'carbon-components-react';
+
+export default function App() {
+  return (
+    <Button>Hello</Button>
+  );
+}
