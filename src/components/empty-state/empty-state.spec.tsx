68c3d9581306ff63570ad4632a711af4d78a3269
ameerthehacker <ameerjhanprof@gmail.com>
2020-03-28 14:17:06 +0530

    feat: add tests for empty state

---
+++ src/components/empty-state/empty-state.spec.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '../../test-util';
+import EmptyState from './empty-state';
+import { fireEvent } from '@testing-library/react';
+
+describe('EmptyState', () => {
+  it('should show no apps are available', () => {
+    const { getByText } = render(
+      <EmptyState onCreateClick={() => null} height="100vh" />
+    );
+
+    expect(getByText('No apps available!')).toBeTruthy();
+  });
+
+  it('should trigger the callback when create is clicked', () => {
+    const onCreateClick = jest.fn();
+    const { getByText } = render(
+      <EmptyState onCreateClick={onCreateClick} height="100vh" />
+    );
+    const btnCreate = getByText('Create');
+
+    fireEvent.click(btnCreate);
+
+    expect(onCreateClick).toHaveBeenCalledTimes(1);
+  });
+});
