72527d465f6542c6c68d3ebc5e2d469864bd98c1
ameerthehacker <ameerjhanprof@gmail.com>
2020-04-05 14:04:02 +0530

    ref: replace react-select with react-autosuggest

--- src/components/version-dropdown/version-dropdown.tsx
+++ src/components/version-dropdown/version-dropdown.tsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react';
-import { useToast } from '@chakra-ui/core';
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  useRef,
+  ReactNode
+} from 'react';
+import { useToast, Input, Box, theme, useColorMode } from '@chakra-ui/core';
 import { ipcRenderer } from '../../services/native';
 import { IPC_CHANNELS } from '../../constants';
 import { IpcRendererEvent } from 'electron';
-import Select from 'react-select';
 import { FormikProps, FieldInputProps } from 'formik';
+import Autosuggest, { RenderSuggestionParams } from 'react-autosuggest';
+import './version-dropdown.css';
 
 const { GET_IMAGE_TAGS } = IPC_CHANNELS;
 
@@ -39,9 +46,89 @@ export default function VersionDropdown({
   form
 }: VersionDropdownProps) {
   const [options, setOptions] = useState<Option[] | undefined>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [suggestions, setSuggestions] = useState<Option[] | undefined>([]);
   const currentPage = useRef(1);
   const toast = useToast();
+  const [inputValue, setInputValue] = useState<string>('');
+  const { colorMode } = useColorMode();
+  const highlightColor = {
+    light: theme.colors.teal[500],
+    dark: theme.colors.blue[500]
+  };
+  const bgColor = {
+    light: theme.colors.white,
+    dark: theme.colors.gray[700]
+  };
+
+  // Teach Autosuggest how to calculate suggestions for any given input value.
+  const getSuggestions = (value: string) => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0
+      ? []
+      : options?.filter(
+          (option) =>
+            option.label.toLowerCase().slice(0, inputLength) === inputValue
+        );
+  };
+
+  // When suggestion is clicked, Autosuggest needs to populate the input
+  // based on the clicked suggestion. Teach Autosuggest how to calculate the
+  // input value for every given suggestion.
+  const getSuggestionValue = (suggestion: Option) => suggestion.label;
+
+  // Use your imagination to render suggestions.
+  const renderSuggestion = (
+    suggestion: Option,
+    { isHighlighted }: RenderSuggestionParams
+  ) => (
+    <Box
+      p={1}
+      px={3}
+      className="suggestions-container"
+      bg={isHighlighted ? highlightColor[colorMode] : ''}
+    >
+      {suggestion.label}
+    </Box>
+  );
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  const onSuggestionsFetchRequested = ({ value }: { value: string }) => {
+    setSuggestions(getSuggestions(value));
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const renderInputComponent = (inputProps: any) => <Input {...inputProps} />;
+
+  const renderSuggestionContainer = ({
+    containerProps,
+    children
+  }: {
+    children: ReactNode;
+    containerProps: any;
+  }) => (
+    <Box position="relative">
+      <Box
+        bg={bgColor[colorMode]}
+        zIndex={2}
+        width="100%"
+        position="absolute"
+        borderRadius={5}
+        borderWidth={suggestions && suggestions.length > 0 ? '1px' : 0}
+        shadow="lg"
+        {...containerProps}
+        className="suggestions-container"
+      >
+        {children}
+      </Box>
+    </Box>
+  );
 
   const onGetImageTagsResult = useCallback(
     (evt: IpcRendererEvent, { res, error }: GetImageTagsResponse) => {
@@ -56,8 +143,6 @@ export default function VersionDropdown({
             return options;
           }
         });
-
-        setIsLoading(false);
 
         // increment the page number
         currentPage.current++;
@@ -76,11 +161,10 @@ export default function VersionDropdown({
         });
       }
     },
-    [setOptions, setIsLoading, image, toast]
+    [setOptions, image, toast]
   );
 
   useEffect(() => {
-    setIsLoading(true);
     ipcRenderer.send(GET_IMAGE_TAGS, { image });
 
     ipcRenderer.on(GET_IMAGE_TAGS, onGetImageTagsResult);
@@ -91,20 +175,25 @@ export default function VersionDropdown({
   }, [onGetImageTagsResult, image]);
 
   return (
-    <Select
-      id={id}
-      value={options?.find((option) => option.value === field.value)}
-      placeholder={isLoading ? 'Loading...' : placeholder}
-      options={options}
-      onChange={(option: any) => form.setFieldValue(field.name, option?.value)}
-      isDisabled={isLoading}
-      onBlur={(evt) => {
-        field.onBlur(evt);
-        // field is no more tidy
-        form.setFieldTouched(field.name, true);
+    <Autosuggest
+      suggestions={suggestions || []}
+      onSuggestionsClearRequested={onSuggestionsClearRequested}
+      onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+      getSuggestionValue={getSuggestionValue}
+      renderSuggestion={renderSuggestion}
+      inputProps={{
+        placeholder,
+        id,
+        ...form,
+        ...field,
+        value: inputValue,
+        onChange: (evt, { newValue }) => {
+          setInputValue(newValue);
+          form.setFieldValue(field.name, newValue);
+        }
       }}
-      isClearable={true}
-      isSearchable={true}
-    ></Select>
+      renderInputComponent={renderInputComponent}
+      renderSuggestionsContainer={renderSuggestionContainer}
+    />
   );
 }
