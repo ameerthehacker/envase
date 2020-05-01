2b8cfdbf680e58bc81b2548c25435d136883a86b
ameerthehacker <ameerjhanprof@gmail.com>
2020-05-01 19:41:11 +0530

    feat: design a basic search box

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,10 +4,12 @@ import {
   IconButton,
   useColorMode,
   Tooltip,
-  useDisclosure
+  useDisclosure,
+  Flex
 } from '@chakra-ui/core';
 import { useApp } from '../../hooks/use-app/use-app';
 import Preferences from '../../screens/preferences/preferences';
+import Search from '../search/search';
 
 export default function Navbar() {
   const { toggleColorMode, colorMode } = useColorMode();
@@ -15,41 +17,44 @@ export default function Navbar() {
   const { isOpen, onClose, onOpen } = useDisclosure();
 
   return (
-    <Box p={2} width="auto" pos="fixed" right={0} top={0}>
-      <Tooltip label="Preferences" aria-label="Open preferences">
-        <IconButton
-          opacity={0.65}
-          fontSize="xl"
-          variant="ghost"
-          icon="settings"
-          onClick={onOpen}
-          aria-label="Open preferences"
-        />
-      </Tooltip>
-      <Tooltip label="Reload apps" aria-label="Reload apps">
-        <IconButton
-          opacity={0.65}
-          fontSize="xl"
-          variant="ghost"
-          onClick={() => load()}
-          icon="repeat"
-          aria-label="reload-apps"
-        />
-      </Tooltip>
-      <Tooltip
-        label={`Turn lights ${colorMode === 'light' ? 'off' : 'on'}`}
-        aria-label="toggle dark/light mode"
-      >
-        <IconButton
-          opacity={0.65}
-          fontSize="xl"
-          variant="ghost"
-          onClick={() => toggleColorMode()}
-          icon={colorMode === 'light' ? 'moon' : 'sun'}
-          aria-label="toggle-dark-mode"
-        />
-      </Tooltip>
-      <Preferences isOpen={isOpen} onClose={onClose} />
-    </Box>
+    <Flex p={2} width="auto" pos="fixed" right={0} top={0}>
+      <Box mr={3}>
+        <Tooltip label="Preferences" aria-label="Open preferences">
+          <IconButton
+            opacity={0.65}
+            fontSize="xl"
+            variant="ghost"
+            icon="settings"
+            onClick={onOpen}
+            aria-label="Open preferences"
+          />
+        </Tooltip>
+        <Tooltip label="Reload apps" aria-label="Reload apps">
+          <IconButton
+            opacity={0.65}
+            fontSize="xl"
+            variant="ghost"
+            onClick={() => load()}
+            icon="repeat"
+            aria-label="reload-apps"
+          />
+        </Tooltip>
+        <Tooltip
+          label={`Turn lights ${colorMode === 'light' ? 'off' : 'on'}`}
+          aria-label="toggle dark/light mode"
+        >
+          <IconButton
+            opacity={0.65}
+            fontSize="xl"
+            variant="ghost"
+            onClick={() => toggleColorMode()}
+            icon={colorMode === 'light' ? 'moon' : 'sun'}
+            aria-label="toggle-dark-mode"
+          />
+        </Tooltip>
+        <Preferences isOpen={isOpen} onClose={onClose} />
+      </Box>
+      <Search onSearch={(s) => console.log(s)} />
+    </Flex>
   );
 }
---
+++ src/components/search/search.tsx
@@ -0,0 +1,45 @@
+import React, { useState, ChangeEvent, KeyboardEvent, useEffect } from 'react';
+import { InputGroup, Input, InputRightElement, Box } from '@chakra-ui/core';
+import { FaSearch, FaTimes } from 'react-icons/fa';
+import { useDebounce } from 'use-debounce';
+
+export interface SearchProps {
+  onSearch: (searchText: string) => void;
+}
+
+export default function Search({ onSearch }: SearchProps) {
+  const [text, setText] = useState('');
+  const [value, , callPending] = useDebounce(text, 1000);
+
+  useEffect(() => {
+    onSearch(value);
+  }, [value]);
+
+  return (
+    <InputGroup>
+      <Input
+        onKeyDown={(evt: KeyboardEvent) => {
+          // escape key
+          if (evt.keyCode === 27) {
+            setText('');
+          }
+          if (evt.keyCode === 13) {
+            callPending();
+          }
+        }}
+        placeholder="Search"
+        value={text}
+        onChange={(evt: ChangeEvent<HTMLInputElement>) =>
+          setText(evt.target.value)
+        }
+      />
+      <InputRightElement>
+        <Box
+          cursor={text.length > 0 ? 'pointer' : 'auto'}
+          onClick={() => setText('')}
+          as={text.length === 0 ? FaSearch : FaTimes}
+        />
+      </InputRightElement>
+    </InputGroup>
+  );
+}
