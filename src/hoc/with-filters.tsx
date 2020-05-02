fdd0999ad05bf1aaa07ceea50f91bbce678bbe56
ameerthehacker <ameerjhanprof@gmail.com>
2020-05-02 16:02:06 +0530

    ref: convert HOC into functional component

--- src/hoc/with-filters.tsx
+++ src/hoc/with-filters.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Categories from '../components/categories/catrgories';
 import { getAllTags } from '../utils/utils';
 import { FORMULAS } from '../formulas';
@@ -9,53 +9,38 @@ export interface FilterProps {
   onFiltersClose: () => void;
 }
 
-interface FilterState {
-  categories: Category;
-}
-
 export default function withFilters<T>(Component: FC<T>) {
   const allCategories = getAllTags(FORMULAS);
 
-  return class ComponentWithFilters extends React.Component<
-    FilterProps & T,
-    FilterState
-  > {
-    constructor(props: FilterProps & T) {
-      super(props);
-      this.state = {
-        categories: allCategories
-      };
-    }
+  return function ComponentWithFilter({
+    isFiltersOpen,
+    onFiltersClose,
+    ...rest
+  }: FilterProps & T) {
+    const [categories, setCategories] = useState(allCategories);
+    const noCategorySelected =
+      Object.keys(categories)
+        .map((category) => categories[category])
+        .filter(Boolean).length === 0;
 
-    render() {
-      const noCategorySelected =
-        Object.keys(this.state.categories)
-          .map((category) => this.state.categories[category])
-          .filter(Boolean).length === 0;
-
-      return (
-        <>
-          <Component
-            selectedCategories={
-              noCategorySelected ? undefined : this.state.categories
-            }
-            {...(this.props as any)}
-          />
-          <Categories
-            onChange={(category) =>
-              this.setState((state) => ({
-                categories: {
-                  ...state.categories,
-                  ...category
-                }
-              }))
-            }
-            isOpen={this.props.isFiltersOpen}
-            onClose={this.props.onFiltersClose}
-            categories={this.state.categories}
-          />
-        </>
-      );
-    }
+    return (
+      <>
+        <Component
+          selectedCategories={noCategorySelected ? undefined : categories}
+          {...(rest as any)}
+        />
+        <Categories
+          onChange={(category) =>
+            setCategories((categories) => ({
+              ...categories,
+              ...category
+            }))
+          }
+          isOpen={isFiltersOpen}
+          onClose={onFiltersClose}
+          categories={categories}
+        />
+      </>
+    );
   };
 }
