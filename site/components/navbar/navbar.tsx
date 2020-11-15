a8370be85eee9db65ecb37870cbc26431457a72f
Ameer Jhan <ameerjhanprof@gmail.com>
2020-11-15 19:28:18 +0530

    feat: add drawer

--- site/components/navbar/navbar.tsx
+++ site/components/navbar/navbar.tsx
@@ -1,58 +1,110 @@
-import { Box, Flex, Image, Text } from '@chakra-ui/core';
-import { ReactNode } from 'react';
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Image,
+  Text,
+  useDisclosure
+} from '@chakra-ui/core';
+import { ReactNode, useRef } from 'react';
 import { GITHUB_REPO } from '../../constants';
 import styles from './navbar.module.css';
 import logo from './logo.png';
 import { MdMenu } from 'react-icons/md';
 
 export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const menuRef = useRef(null);
+
   return (
-    <Flex
-      alignItems="center"
-      position="fixed"
-      top={0}
-      height="60px"
-      width="100%"
-      justifyContent="center"
-      bg="white"
-      marginLeft={3}
-    >
-      <Flex fontSize="md" gridColumn={2}>
-        <Flex alignItems="center">
-          <Box
-            cursor="pointer"
-            visibility={['visible', 'visible', 'visible', 'hidden']}
-            fontSize="5xl"
-            as={MdMenu}
-            marginRight={2}
-          />
-          <BrandLogo />
-        </Flex>
-        <Flex
-          marginLeft={10}
-          fontSize="lg"
-          alignItems="center"
-          className={styles.navLinks}
-          visibility={['hidden', 'hidden', 'hidden', 'visible']}
-        >
-          <NavLink href="#why-envase">🤷‍♀️ Why Envase?</NavLink>
-          <NavLink href="#solutions">🔨 Solutions</NavLink>
-          <NavLink isExternal={true} href={`https://github.com/${GITHUB_REPO}`}>
-            💻 View Source
-          </NavLink>
+    <>
+      <Flex
+        alignItems="center"
+        position="fixed"
+        top={0}
+        height="60px"
+        width="100%"
+        justifyContent="center"
+        bg="white"
+        paddingLeft={[10, 10, 10, 0]}
+      >
+        <Flex fontSize="md" gridColumn={2}>
+          <Flex alignItems="center">
+            <Box
+              ref={menuRef}
+              cursor="pointer"
+              onClick={onOpen}
+              visibility={['visible', 'visible', 'visible', 'hidden']}
+              fontSize="5xl"
+              as={MdMenu}
+            />
+            <BrandLogo />
+          </Flex>
+          <Flex
+            marginLeft={10}
+            fontSize="lg"
+            alignItems="center"
+            className={styles.navLinks}
+            visibility={['hidden', 'hidden', 'hidden', 'visible']}
+          >
+            <NavLink href="#why-envase">🤷‍♀️ Why Envase?</NavLink>
+            <NavLink href="#solutions">🔨 Solutions</NavLink>
+            <NavLink
+              isExternal={true}
+              href={`https://github.com/${GITHUB_REPO}`}
+            >
+              💻 View Source
+            </NavLink>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={menuRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+          <DrawerBody>
+            <MenuLink onClick={onClose} href="#why-envase" paddingTop={0}>
+              🤷‍♀️ Why Envase?
+            </MenuLink>
+            <MenuLink onClick={onClose} href="#solutions">
+              🔨 Solutions
+            </MenuLink>
+            <MenuLink
+              onClick={onClose}
+              isExternal={true}
+              href={`https://github.com/${GITHUB_REPO}`}
+            >
+              💻 View Source
+            </MenuLink>
+          </DrawerBody>
+          <DrawerFooter></DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
 
-type NavLinkProps = {
+type LinkProps = {
   href: string;
   children: ReactNode;
   isExternal?: boolean;
+  paddingTop?: number;
+  onClick?: () => void;
 };
 
-function NavLink({ href, children, isExternal }: NavLinkProps) {
+function NavLink({ href, children, isExternal }: LinkProps) {
   return (
     <a
       className={styles.navLink}
@@ -61,6 +113,16 @@ function NavLink({ href, children, isExternal }: NavLinkProps) {
     >
       {children}
     </a>
+  );
+}
+
+function MenuLink({ href, children, isExternal, ...rest }: LinkProps) {
+  return (
+    <Box paddingTop={2} paddingBottom={2} {...rest}>
+      <a href={href} target={isExternal ? 'blank' : ''}>
+        {children}
+      </a>
+    </Box>
   );
 }
 
