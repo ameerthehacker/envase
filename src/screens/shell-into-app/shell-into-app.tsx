bce1f256175f5bf0724cf4a7998f1724ad5f11e1
Ameer Jhan <ameerjhanprof@gmail.com>
2020-11-16 15:03:16 +0530

    feat: start implementing tabs in shell

--- src/screens/shell-into-app/shell-into-app.tsx
+++ src/screens/shell-into-app/shell-into-app.tsx
@@ -1,49 +1,104 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import {
   shellOrExecIntoApp,
   getContainerAppInfo
 } from '../../services/docker/docker';
 import Terminal from '../../components/terminal/terminal';
-import { Box, useToast } from '@chakra-ui/core';
+import {
+  Box,
+  Flex,
+  IconButton,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  useToast
+} from '@chakra-ui/core';
 import { parse } from 'query-string';
 import { Helmet } from 'react-helmet';
 
 export default function ShellIntoApp() {
   const { containerId } = useParams<{ containerId: string }>();
+  const currentId = useRef(1);
+  const [shellTabs, setShellTabs] = useState([
+    {
+      containerId,
+      id: currentId.current
+    }
+  ]);
   const location = useLocation();
-  const toast = useToast();
   const queryParams = parse(location.search);
   const cmd = Array.isArray(queryParams.cmd)
     ? queryParams.cmd[0]
     : queryParams.cmd;
 
+  return (
+    <Tabs size="sm" variant="enclosed">
+      <Flex>
+        <TabList>
+          {shellTabs.map((shellTab, index) => (
+            <Tab key={shellTab.id}>Shell {index + 1}</Tab>
+          ))}
+        </TabList>
+        <IconButton
+          onClick={(evt) => {
+            currentId.current += 1;
+
+            setShellTabs((tabs) => [
+              ...tabs,
+              { containerId, id: currentId.current }
+            ]);
+          }}
+          size="sm"
+          variant="ghost"
+          aria-label="add-tab"
+          icon="add"
+        />
+      </Flex>
+      <TabPanels>
+        {shellTabs.map((shellTab) => (
+          <TabPanel key={shellTab.id}>
+            <Shell cmd={cmd} containerId={containerId} />
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  );
+}
+
+type ShellProps = {
+  containerId: string;
+  cmd: string | null;
+};
+
+function Shell({ containerId, cmd }: ShellProps) {
+  const toast = useToast();
   const [stream, setStream] = useState<NodeJS.ReadWriteStream>();
   const [appName, setAppName] = useState<string>();
 
   useEffect(() => {
-    if (containerId) {
-      getContainerAppInfo(containerId).then((containerAppInfo) => {
-        setAppName(containerAppInfo.formValues.name);
-      });
-      shellOrExecIntoApp(containerId, cmd)
-        .then((exec) => {
-          exec.start(
-            { hijack: true, stdin: true, Tty: true },
-            (err: any, stream: NodeJS.ReadWriteStream | undefined) => {
-              setStream(stream);
-            }
-          );
-        })
-        .catch((err) => {
-          toast({
-            title: 'Unable to shell',
-            description: `${err}`,
-            status: 'error',
-            isClosable: true
-          });
+    getContainerAppInfo(containerId).then((containerAppInfo) => {
+      setAppName(containerAppInfo.formValues.name);
+    });
+    shellOrExecIntoApp(containerId, cmd)
+      .then((exec) => {
+        exec.start(
+          { hijack: true, stdin: true, Tty: true },
+          (err: any, stream: NodeJS.ReadWriteStream | undefined) => {
+            setStream(stream);
+          }
+        );
+      })
+      .catch((err) => {
+        toast({
+          title: 'Unable to shell',
+          description: `${err}`,
+          status: 'error',
+          isClosable: true
         });
-    }
+      });
   }, [containerId, cmd, toast]);
 
   return (
@@ -51,7 +106,7 @@ export default function ShellIntoApp() {
       <Helmet>
         <title>{`Terminal${appName ? ` [${appName}]` : ''}`}</title>
       </Helmet>
-      <Box height="100vh" bg="black">
+      <Box height="calc(100vh - 29px)" bg="black">
         {stream && <Terminal stdin={true} stream={stream} />}
       </Box>
     </>
