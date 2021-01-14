a9138482cc8d1f11f1fd27c7fa678e89404f77bc
Ameer Jhan <ameerjhanprof@gmail.com>
2021-01-14 17:28:01 +0530

    feat: add why envase section

--- site/components/why-envase/why-envase.tsx
+++ site/components/why-envase/why-envase.tsx
@@ -1,20 +1,19 @@
-import { Box, Flex, Text } from '@chakra-ui/core';
+import { Text } from '@chakra-ui/core';
 import Section from '../section/section';
 
 export default function WhyEnvase() {
   return (
     <Section bg="#fff">
       <Text fontSize={['2xl', '4xl']}>🤷‍♀️ Why Envase?</Text>
-      <Text fontSize="xl">
+      <Text marginTop={1} fontSize="lg">
         Using docker for development is <strong>not easy</strong> as it seems,
         you need to search for the right container image in DockerHub and then
         read the <strong>lengthy, long and boring documentation</strong> to
-        understand how to run it with all the right configurations, even then
-        remembering which port that the container exposed, starting and stoping
-        the container are nightmares for someone who is new to Docker. If you
-        need to run containers which depened on other like{' '}
-        <strong>Wordpress</strong> needs <strong>MySQL</strong> frustrates even
-        experienced developers.
+        understand how to run it with all the right configurations are
+        nightmares for someone who is new to Docker. If you need to run
+        containers which depened on other like <strong>Wordpress</strong> needs{' '}
+        <strong>MySQL</strong> frustrates even experienced developers. Envase
+        lets you to do all the above without tears 😢
       </Text>
     </Section>
   );
